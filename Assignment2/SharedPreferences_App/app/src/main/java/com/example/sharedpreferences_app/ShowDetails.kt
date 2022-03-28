9d97bee730e120ecac58e87b2097b236c02effea
tchamalsha <tharu.chamalsha@gmail.com>
2022-03-28 20:06:22 +0530

    assigment2

---
+++ Assignment2/SharedPreferences_App/app/src/main/java/com/example/sharedpreferences_app/ShowDetails.kt
@@ -0,0 +1,11 @@
+package com.example.sharedpreferences_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_details)
+    }
+}
